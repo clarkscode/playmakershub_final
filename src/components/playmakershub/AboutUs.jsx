f6bb86bd4f935d1b0c0177d7668de8a497bb29be
jhrocales <jhrocales@gmail.com>
2024-11-30 18:01:10 +0800

    refined AboutUs.jsx, added logo and responsiveness

--- src/components/playmakershub/AboutUs.jsx
+++ src/components/playmakershub/AboutUs.jsx
@@ -1,48 +1,48 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaQuestion, FaExclamation } from "react-icons/fa";
+
+
+// Importing the images
+import CurricularImg from "../../assets/AboutUs/Curricular.png";
+import NonCurricularImg from "../../assets/AboutUs/Non-curricular.png";
+import USGImg from "../../assets/AboutUs/USG.png";
 
 const AboutUs = () => {
    const navigate = useNavigate();
    return (
       <div className="font-lexend bg-Radial bg-[#000000] text-white h-screen">
          {/* Header */}
-         <header className="flex items-center justify-between p-4 shadow-md py-1 space-x-10 pt-3">
+         <header className="flex items-center justify-between p-4 shadow-md py-1 space-x-10 pt-3 pb-4">
             <div className="flex items-center pl-3 w-3/5">
-               <img src="playmakerslogo.png" alt="Logo" className="w-16 h-16" />
-               <p className="text-2xl font-medium ml-4">Playmakers Hub</p>
+               <img src="playmakerslogo.png" alt="Logo" className="w-20 h-20" />
+               <p className="text-3xl font-bold ml-4">Playmakers Hub</p>
             </div>
             <nav className="flex justify-center space-x-20">
                <a
                   onClick={() => navigate("/")}
-                  className="text-[#FFFFFF] text-2xl font-medium hover:text-[#a83c70] cursor-pointer"
+                  className="text-[#FFFFFF] text-2xl font-semibold hover:text-[#a83c70] cursor-pointer"
                >
                   Home
-               </a>
-               <a
-                  onClick={() => navigate("/homepage/event/approved")}
-                  className="text-[#FFFFFF] text-2xl font-medium hover:text-[#a83c70] cursor-pointer"
-               >
-                  Events
-               </a>
-               <a
-                  onClick={() => navigate("/join")}
-                  className="text-[#FFFFFF] text-2xl font-medium hover:text-[#a83c70] cursor-pointer"
-               >
-                  Join Us
                </a>
             </nav>
          </header>
 
          {/* Main Content */}
-         <div className="bg-Arial px-8 py-10 bg-Radial bg-[#000000]">
+         <div className="bg-lexend px-8 py-10 bg-Radial bg-[#000000]">
             <div className="py-10">
-               <h2 className="text-center text-3xl font-bold -mt-10 mb-8">
+               <h2 className="text-center text-5xl font-bold -mt-10 mb-8 font-lexend">
                   Types of Organizations
                </h2>
-               <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-lg">
-                  <div>
+               <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-xl">
+                  <div className="flex flex-col items-center">
+                     <img
+                        src={USGImg}
+                        alt="USG"
+                        className="w-40 h-40"
+                     />
                      <p className="text-justify">
-                        <span className="font-semibold">
+                        <span className="font-extrabold">
                            University Student Government
                         </span>{" "}
                         is the highest student governing body of the University of
@@ -53,9 +53,14 @@ const AboutUs = () => {
                         lives.
                      </p>
                   </div>
-                  <div>
+                  <div className="flex flex-col items-center">
+                     <img
+                        src={CurricularImg}
+                        alt="Curricular"
+                        className="w-40 h-40"
+                     />
                      <p className="text-justify">
-                        <span className="font-semibold">
+                        <span className="font-extrabold">
                            Curricular Student Organizations
                         </span>{" "}
                         are aligned with academic disciplines and provide support and
@@ -67,9 +72,14 @@ const AboutUs = () => {
                         exhibits and other learning activities.
                      </p>
                   </div>
-                  <div>
+                  <div className="flex flex-col items-center">
+                     <img
+                        src={NonCurricularImg}
+                        alt="Non-Curricular"
+                        className="w-40 h-40"
+                     />
                      <p className="text-justify">
-                        <span className="font-semibold">
+                        <span className="font-extrabold">
                            Non-Curricular Student Organizations
                         </span>{" "}
                         focus on extracurricular interests such as arts, culture,
@@ -87,42 +97,36 @@ const AboutUs = () => {
             {/* Links Section */}
             <section className="py-10">
                <div id="about" className="mb-10 flex items-start">
-                  <i className="fa fa-question-circle text-6xl mr-6"></i>
-                  <p className="text-lg">
-                     The Playmakers-USTP Music Organization, founded by Mark Gonzales
-                     in 2013 and currently having Engr. Jerry Halibas as a coordinator,
-                     is part of the Arts and Culture Division. With its 80 dedicated
-                     members, Playmakers - USTP is responsible for organizing and
-                     managing participants for music-related campus events such as
-                     performances, concerts, and music competitions.
+                  {/* Icon with Circular Background */}
+                  <div className="flex-shrink-0 flex items-center justify-center mr-4 bg-gray-800 rounded-full w-20 h-20">
+                     <FaQuestion className="text-3xl text-white" />
+                  </div>
+                  <p className="text-xl leading-relaxed">
+                     The Playmakers-USTP Music Organization, founded by Mark Gonzales in 2013
+                     and currently having Engr. Jerry Halibas as a coordinator, is part of the
+                     Arts and Culture Division. With its 80 dedicated members, Playmakers -
+                     USTP is responsible for organizing and managing participants for
+                     music-related campus events such as performances, concerts, and music
+                     competitions.
                   </p>
                </div>
+
                <div id="events" className="mb-10 flex items-start">
-                  <i className="fa fa-calendar text-6xl mr-6"></i>
-                  <p className="text-lg">
-                     Playmakers manages around three to four events each month, which
-                     is a significant workload considering the members are also
-                     students with academic responsibilities and having a frequency of
-                     three times a week practices for an upcoming event. Below is an
-                     example of an event done by members.
-                  </p>
-               </div>
-               <div className="mt-10 flex items-start">
-                  <i className="fa fa-user-plus text-6xl mr-6"></i>
-                  <p className="text-lg">
-                     For details on joining the organization, follow our Facebook page
-                     by clicking{" "}
-                     <a
-                        href="https://www.facebook.com/PlaymakersMusicOrganization"
-                        className="underline font-bold"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        this link.
-                     </a>
+                  {/* Icon with Circular Background */}
+                  <div className="flex-shrink-0 flex items-center justify-center mr-4 bg-gray-800 rounded-full w-20 h-20">
+                     <FaExclamation className="text-3xl text-white" />
+                  </div>
+                  <p className="text-xl leading-relaxed">
+                     Playmakers manages around three to four events each month, which is a
+                     significant workload considering the members are also students with
+                     academic responsibilities and having a frequency of three times a week
+                     practices for an upcoming event. Examples of events include the Opening
+                     Act for Intramurals, SwiftiesNight 2023, SCITC General Assembly, and more.
                   </p>
                </div>
             </section>
+
+
          </div>
       </div>
    );
